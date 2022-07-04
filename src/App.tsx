af2f22882dfbfbee373434a48c416a1a83131220
miqueiasbelfort <kawasousa1554@gmail.com>
2022-07-04 15:00:43 -0300

    header & footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import './App.css'
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+
+import styles from "./App.module.css"
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Navbar/>
+      <main className={styles.main}>
+        <h1>Counteudo...</h1>
+      </main>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import styles from './Navbar.module.css'
+
+function Navbar() {
+  return (
+    <header className={styles.header}>
+        <h1>React + TS Todo</h1>
+    </header>
+  )
+}
+
+export default Navbar
